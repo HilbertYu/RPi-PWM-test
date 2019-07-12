57b06cd6202c5f769949afaba710a2a1cef6ac43
Hsin-Hsuan Yu <hsinhsuanyu@gmail.com>
2019-07-13 03:02:36 +0800

    * test2

--- main.c
+++ main.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
+const int pin = 18;
 
 
 int main(int argc, const char * argv[])
 {
+    /* int ret = wiringPiSetupGpio(); */
+    /* printf("ret = %d\n", ret); */
     wiringPiSetupGpio();
-//    wiringPiSetup();
-    pinMode(18, OUTPUT);
-    digitalWrite(18, 0);
-   // delay(3000);
 
-    pinMode(18, PWM_OUTPUT);
+    printf("ok\n");
+
+    pinMode(18, OUTPUT);
+/*     pinMode(18, OUTPUT); */
+/*  //   pinMode(18, INPUT); */
+/*     digitalWrite(18, 1); */
+/*     delay(1000); */
+/*     return 0; */
+    /* digitalWrite(18, 0); */
+    /* delay(3000); */
+    /* return 0; */
+
+    pinMode(pin, PWM_OUTPUT);
     pwmSetMode(PWM_MODE_MS);
   //  pwmSetMode(PWM_MODE_BAL);
-    pwmSetClock(1920); //10Khz
+    //19.2 M = 19200 K
+    pwmSetClock(1920*2); //10Khz
     pwmSetRange(100); //10Hz
 
     while (1)
     {
 
-        pwmWrite(18, 1);
-        delay(1);
+        pwmWrite(pin, 10);
+        //delay(10*1000);
 
     }
 
+    return 0;
 
     pwmWrite(18, 500);
     delay(1000);
