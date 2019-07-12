f4df08cc9b61e5dd7a8bbb9af7b6894954cb0931
Hsin-Hsuan Yu <hsinhsuanyu@gmail.com>
2019-07-13 01:24:13 +0800

    * 100 Hz

--- main.c
+++ main.c
@@ -19,26 +19,9 @@ int main(int argc, const char * argv[])
 
     while (1)
     {
-       // pwmSetRange(1000); //10Hz
 
-        int t = 0;
-
-        for (t = 0; t < 3; ++t)
-        {
-
-            int i = 0;
-            for (i = 1; i < 1000; ++i)
-            {
-                pwmWrite(18, i);
-                delay(2);
-            }
-
-            for (i = 1000; i >= 1; --i)
-            {
-                pwmWrite(18, i);
-                delay(2);
-            }
-        }
+        pwmWrite(18, 1);
+        delay(1);
 
     }
 
