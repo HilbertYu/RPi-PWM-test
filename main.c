d21dc52f3bc21bf837c11c68c12c980ad15b6411
Hsin-Hsuan Yu <hsinhsuanyu@gmail.com>
2019-07-13 12:15:46 +0800

    * pre

--- main.c
+++ main.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <wiringPi.h>
 
 const int pin = 18;
 
+void init(void)
+{
+    if (wiringPiSetup() < 0)
+    {
+        fprintf(stderr, "init fail\n");
+        exit(1);
+    }
+
+    if (wiringPiSetupGpio() < 0)
+    {
+        fprintf(stderr, "init fail\n");
+        exit(1);
+    }
+
+}
+
 
 int main(int argc, const char * argv[])
 {
-    /* int ret = wiringPiSetupGpio(); */
-    /* printf("ret = %d\n", ret); */
-    wiringPiSetup();
-    wiringPiSetupGpio();
 
-    printf("init\n");
+    init();
+    printf("init - done\n");
 
     pinMode(pin, OUTPUT);
 
@@ -29,30 +43,27 @@ int main(int argc, const char * argv[])
     pwmSetClock(dc);
     pwmSetRange(rng);
 
-
     const double C = 1920*1000;
     printf("pwm c = %.2lf Hz\n", (double)C/(dc*rng));
 
     if (1920*2 % mul != 0)
     {
         printf("!!\n");
-
     }
 
-
-
     {
-        const int wt = 1;
+        const int wt = 2;
 
         pwmWrite(pin, 0);
         int i = 0;
-        for (i = 0; i < wt; ++i)
+        for (i = 0; i < wt + 2; ++i)
         {
             printf("(0) i = %d\n", i);
             delay(1000);
         }
 
         pwmWrite(pin, 3*mul);
+
         for (i = 0; i < wt; ++i)
         {
             printf("(3) i = %d\n", i);
