3b17eb879d461255e1e56b94c12c799de4a3a174
Hsin-Hsuan Yu <hsinhsuanyu@gmail.com>
2019-07-13 12:08:58 +0800

    * preci

--- main.c
+++ main.c
@@ -11,52 +11,73 @@ int main(int argc, const char * argv[])
     wiringPiSetup();
     wiringPiSetupGpio();
 
-    printf("ok\n");
+    printf("init\n");
 
-    pinMode(18, OUTPUT);
+    pinMode(pin, OUTPUT);
 
     pinMode(pin, PWM_OUTPUT);
     pwmSetMode(PWM_MODE_MS);
   //  pwmSetMode(PWM_MODE_BAL);
     //19.2 M = 19200 K
-    pwmSetClock(1920*2); //10Khz
-    pwmSetRange(100); //10Hz
+
+
+    const int mul = 20*3*8;
+
+    const int dc = 1920*2/mul;
+    const int rng = 100*mul;
+
+    pwmSetClock(dc);
+    pwmSetRange(rng);
+
+
+    const double C = 1920*1000;
+    printf("pwm c = %.2lf Hz\n", (double)C/(dc*rng));
+
+    if (1920*2 % mul != 0)
+    {
+        printf("!!\n");
+
+    }
+
 
 
     {
+        const int wt = 1;
+
         pwmWrite(pin, 0);
         int i = 0;
-        for (i = 0; i < 10; ++i)
+        for (i = 0; i < wt; ++i)
         {
             printf("(0) i = %d\n", i);
             delay(1000);
         }
 
-        pwmWrite(pin, 3);
-        for (i = 0; i < 10; ++i)
+        pwmWrite(pin, 3*mul);
+        for (i = 0; i < wt; ++i)
         {
             printf("(3) i = %d\n", i);
             delay(1000);
         }
     }
 
-    double i = 4;
     while (1)
     {
-        while (i < 10)
+        double i = 4 * mul;
+
+        while (i < 10 * mul)
         {
-            printf("i1 = %.2lf\n", i);
+            printf("i1 = %.2lf, %.2lf\n", i, i/mul);
             pwmWrite(pin, i);
             delay(50);
-            i += 0.2;
+            i += (0.02 * mul);
         }
 
-        while (i > 4)
+        while (i > 4 * mul)
         {
-            printf("i2 = %.2lf\n", i);
+            printf("i2 = %.2lf, %.2lf\n", i, i/mul);
             pwmWrite(pin, i);
             delay(50);
-            i -= 0.5;
+            i -= (0.05 * mul);
         }
         //delay(10*1000);
 
